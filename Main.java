c3ca650214765fd5cf6a267adafa904b19e4f2bd
Emanuel Mesa <mesaemanuel922@gmail.com>
2024-05-29 07:22:03 -0500

    c1

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.cesde;
+
+public class Main {
+    public static void main(String[] args) {
+        Ejercicios ejercicios = new Ejercicios();
+
+        System.out.println("Ejercicio 1");
+        System.out.println(ejercicios.ejercicio1(18, 27));
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Ejercicio 2");
+        System.out.println(ejercicios.ejercicio2(2.6));
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Ejercicio 3");
+        System.out.println(ejercicios.ejercicio3(6300));
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Ejercicio 4");
+        System.out.println(ejercicios.ejercicio4(1, 3, 2, 3, 1));
+        System.out.println("-------------------------------------------------");
+
+        System.out.println("Ejercicio 5");
+        System.out.println(ejercicios.ejercicio5(4,7,9));
+        System.out.println("-------------------------------------------------");
+
+    }
+}
